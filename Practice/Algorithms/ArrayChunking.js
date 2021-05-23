7bd864c47fa029ec08fb1ea6c622514d5173bf3c
Winston Chen <winstonchen.wm@gmail.com>
2021-05-22 23:46:39 -0400

    Array Chunking

---
+++ Practice/Algorithms/ArrayChunking.js
@@ -0,0 +1,9 @@
+/* 
+Directions:
+Given an array and chunk size, divide the array into many subarrays where each subarray is of length size
+
+Example:
+chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]] 
+*/
+
+//Solution 1
