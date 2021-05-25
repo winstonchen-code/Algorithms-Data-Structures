250ef51e5ff225ec26248cc03ada39a098ec611f
Winston Chen <winstonchen.wm@gmail.com>
2021-05-25 13:56:28 -0400

    String Reversal Solution 1

--- Practice/Algorithms/StringReversal.js
+++ Practice/Algorithms/StringReversal.js
@@ -1,13 +1,33 @@
-/* 
+/*
+--- Directions
 Given a string, return a new string with the reversed order of characters.
 
-Examples:
-
-reverse('aaple') === 'leppa'
+--- Examples
+reverse('apple') === 'leppa'
 reverse('hello') === 'olleh'
-reverse('Greetings!') === '!sgniterrG'
+reverse('Greetings!') === '!sgniteerG'
 */
 
+//Tip: The reverse() method reverses an array in place. 
+
+//Solution 1
+
 function reverse(str) {
-    return str.split('').reverse().join('') 
+    const arr = str.split('');
+    arr.reverse();
+    return arr.join('');
 }
+
+//Clean up of Solution 1
+
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+//Solution 2 (For Loop)
+
+function reverse(str) {
+    const arr = str.split('');
+}
+
+//Solution 3 (Array Helper)
