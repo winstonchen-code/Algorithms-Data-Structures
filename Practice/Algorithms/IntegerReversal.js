1827d1c345cb16b0813385c049c94986a47ae917
Winston Chen <winstonchen.wm@gmail.com>
2021-05-24 23:56:16 -0400

    Integer Reversal Update

--- Practice/Algorithms/IntegerReversal.js
+++ Practice/Algorithms/IntegerReversal.js
@@ -8,3 +8,7 @@ reverseInt(981) === 189;
 reverseInt(500) === 5;
 */
 
+function reverseInt(n) {
+    const reversed = n.toString().split('').reverse().join('');
+    return parseInt(reversed) * Math.sign(n);
+}
