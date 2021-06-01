783650c8199594501bbd528b1135d4728f7fa5db
Winston Chen <winstonchen.wm@gmail.com>
2021-06-01 02:51:16 -0400

    Update

--- Practice/Algorithms/Anagrams.js
+++ Practice/Algorithms/Anagrams.js
@@ -1,3 +1,15 @@
+/* 
+--- Directions
+Check to see if two provided strings are anagrams of each other. 
+One string is an anagram of another if it uses the same characters in the same quanitty. Only consider characters, not spaces or punctuation.
+Consider capital letters to be the same a lower case.
+
+--- Examples
+    anagrams('rail safety', 'fairy tales') --> True
+    anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+    anagrams('Hi there', 'Bye there') --> False
+*/
+
 //Solution 1
 
 function anagrams(stringA, stringB) {
