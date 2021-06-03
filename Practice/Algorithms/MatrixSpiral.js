247a39fd89e94c6a8a77833d4b7f02b6f23c560e
Winston Chen <winstonchen.wm@gmail.com>
2021-06-03 01:13:18 -0400

    Matrix Spiral

--- Practice/Algorithms/MatrixSpiral.js
+++ Practice/Algorithms/MatrixSpiral.js
@@ -1,13 +1,20 @@
+/*
+--- Directions
+Write a function that accepts an integer N and returns a NxN spiral matrix.
 
-function vowels(str) {
-    let count = 0;
-    const theVowels = 'aeiou';
+--- Examples
+matrix(2)
+    [[1, 2],
+    [4, 3]]
+matrix(3)
+    [[1, 2, 3],
+    [8, 9, 4],
+    [7, 6, 5]]
+matrix(4)
+    [[1, 2, 3, 4],
+    [12, 13, 14, 5],
+    [11, 16, 15, 6],
+    [10, 9, 8, 7]]
+*/
 
-    for (let char of str.toLowerCase()) {
-        if (theVowels.includes(char)) {
-            count++
-        }
-    }
 
-    return count;
-}
