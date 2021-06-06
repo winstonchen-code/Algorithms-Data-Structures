ec4de950cb6bb247513c4bca428b075ba86ef5b2
Winston Chen <winstonchen.wm@gmail.com>
2021-06-05 23:47:20 -0400

    Flatten Array

---
+++ Practice/Extra Algorithms/FlattenArray.js
@@ -0,0 +1,11 @@
+/*
+--- Directions
+Take an array of arrays and flatten to a single array
+
+--- Example
+ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
+*/
+
+function flattenArray(arrays) {
+    
+}
